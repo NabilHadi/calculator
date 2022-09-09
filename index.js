dc2e4a6538ba23ac6fe17fc466d6a7e1690c2a50
Nabil Hadi <napilfadl@gmail.com>
2022-09-09 14:57:20 +0200

    Hook buttons to calculator and Make them work

--- index.js
+++ index.js
@@ -27,3 +27,79 @@ const operate = (operator, x, y) => {
       return "Invalid operator";
   }
 };
+
+const display = document.querySelector(".display");
+display.textContent = "";
+
+function digitButtonClickHandler(e) {
+  const currContent = Number(display.textContent);
+  if (currContent === 0) {
+    display.textContent = e.target.dataset.digit;
+  } else {
+    display.textContent += e.target.dataset.digit;
+  }
+}
+
+function isOperator(char) {
+  switch (char) {
+    case operators.add:
+    case operators.subtract:
+    case operators.multiply:
+    case operators.divide:
+      return true;
+    default:
+      return false;
+  }
+}
+
+function functionButtonClickHandler(e) {
+  const operation = e.target.dataset.function;
+  if (operation === "c") {
+    display.textContent = "";
+  } else if (operation === "=") {
+    let ops = display.textContent.split(/(\*|\+|\/|-)/gi);
+    if (ops.length < 3 || ops[2] === "") return;
+
+    let ops2 = [];
+    for (let i = 0; i < ops.length; i += 2) {
+      if (ops[i + 1] === operators.add || ops[i + 1] === operators.subtract) {
+        ops2.push(ops[i], ops[i + 1]);
+      } else {
+        if (ops[i + 2]) {
+          ops[i + 2] = operate(ops[i + 1], ops[i], ops[i + 2]);
+        } else {
+          ops2.push(ops[i]);
+        }
+      }
+    }
+
+    let currOperation = "";
+    const result = ops2.reduce((total, currElm) => {
+      if (isOperator(currElm)) {
+        currOperation = currElm;
+      } else {
+        if (currElm === "") return total;
+        total = operate(currOperation, Number(total), Number(currElm));
+      }
+      return total;
+    });
+    display.textContent = result;
+  } else {
+    const textLength = display.textContent.length;
+    if (textLength === 0 || isOperator(display.textContent[textLength - 1]))
+      return;
+    display.textContent += operation;
+  }
+}
+
+const digitButtons = Array.from(document.querySelectorAll(".digit-button"));
+digitButtons.forEach((digitBtn) => {
+  digitBtn.addEventListener("click", digitButtonClickHandler);
+});
+
+const functionButtons = Array.from(
+  document.querySelectorAll(".function-button")
+);
+functionButtons.forEach((funcBtn) => {
+  funcBtn.addEventListener("click", functionButtonClickHandler);
+});

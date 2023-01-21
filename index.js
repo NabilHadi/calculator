f98de22e43f2cfdbb32bccee1f86708825c00eef
Nabil Hadi <napilfadl@gmail.com>
2023-01-21 09:07:58 +0200

    Add calculator logic

--- index.js
+++ index.js
@@ -17,14 +17,98 @@ function divide(a, b) {
 function operate(operator = "+", a, b) {
   switch (operator) {
     case "+":
-      return add(a, b);
+      return Math.round(1000 * add(a, b)) / 1000;
     case "-":
-      return subtract(a, b);
+      return Math.round(1000 * subtract(a, b)) / 1000;
     case "*":
-      return multiply(a, b);
+      return Math.round(1000 * multiply(a, b)) / 1000;
     case "/":
-      return divide(a, b);
+      return Math.round(1000 * divide(a, b)) / 1000;
     default:
       return "Unknown";
   }
 }
+
+let storedNum = null;
+let currentOperator = null;
+let displayValue = "0";
+const display = document.querySelector(".display");
+
+function updateDisplay() {
+  display.textContent = displayValue;
+}
+
+function clearState() {
+  storedNum = null;
+  currentOperator = null;
+  displayValue = "0";
+  updateDisplay();
+}
+
+function backspaceHandler() {
+  if (displayValue === "0") return;
+  displayValue = displayValue + "";
+  displayValue = displayValue.substring(0, displayValue.length - 1);
+  if (displayValue.length === 0) {
+    displayValue = "0";
+  }
+  updateDisplay();
+}
+
+function calcBtnClickHandler(event) {
+  const btnValue = event.target.dataset.value;
+  console.log(btnValue);
+  if (event.target.dataset.type === "digit") {
+    if (displayValue == "0") {
+      displayValue = btnValue;
+    } else {
+      displayValue += btnValue;
+    }
+    updateDisplay();
+  } else if (event.target.dataset.type === "operator") {
+    if (!currentOperator) {
+      currentOperator = btnValue;
+      storedNum = displayValue;
+      displayValue = "0";
+      updateDisplay();
+    } else if (storedNum) {
+      if (Number(displayValue) === 0 && currentOperator === "/") {
+        display.textContent = "OOPS! division by 0";
+        return;
+      }
+      displayValue = operate(
+        currentOperator,
+        Number(storedNum),
+        Number(displayValue)
+      );
+      updateDisplay();
+      storedNum = displayValue;
+      displayValue = "0";
+      currentOperator = btnValue;
+    }
+  } else if (btnValue === "clear") {
+    clearState();
+  } else if (btnValue === "=") {
+    if (Number(displayValue) === 0 && currentOperator === "/") {
+      clearState();
+      display.textContent = "OOPS! division by 0";
+      return;
+    }
+    if (!currentOperator) return;
+    displayValue = operate(
+      currentOperator,
+      Number(storedNum),
+      Number(displayValue)
+    );
+    updateDisplay();
+    storedNum = displayValue;
+    currentOperator = null;
+  } else if (btnValue === "backspace") {
+    backspaceHandler();
+  }
+}
+
+const calcBtns = document.querySelectorAll(".calc-btn");
+calcBtns.forEach((btn) => {
+  btn.addEventListener("click", calcBtnClickHandler);
+});
